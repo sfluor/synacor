3e8fa0ab5199df6a3232f3d772f655f5cb7396a9
Tabet Sami <salph.tabet@gmail.com>
2018-01-04 12:40:59 +0100

    fix

--- src/coins.go
+++ src/coins.go
@@ -26,6 +26,7 @@ func main() {
 						if a+b*c*c+d*d*d-e == result &&
 							!(a == b || a == c || a == d || a == e || b == c || b == d || b == e || c == d || c == e || d == e) {
 							fmt.Println(strings.Join([]string{coins[a], coins[b], coins[c], coins[d], coins[e]}, " | "))
+							// > blue coin | red coin | shiny coin | concave coin | corroded coin
 							os.Exit(0)
 						}
 					}
