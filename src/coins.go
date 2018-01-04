4d6ae72e9405329038b383207822812509c2ed08
Tabet Sami <salph.tabet@gmail.com>
2018-01-04 12:40:45 +0100

    added coin door solution

---
+++ src/coins.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	result := 399
+
+	coins := map[int]string{
+		2: "red coin",
+		3: "corroded coin",
+		5: "shiny coin",
+		7: "concave coin",
+		9: "blue coin",
+	}
+
+	// Ugly solution but the problem is small
+	for a := range coins {
+		for b := range coins {
+			for c := range coins {
+				for d := range coins {
+					for e := range coins {
+						if a+b*c*c+d*d*d-e == result &&
+							!(a == b || a == c || a == d || a == e || b == c || b == d || b == e || c == d || c == e || d == e) {
+							fmt.Println(strings.Join([]string{coins[a], coins[b], coins[c], coins[d], coins[e]}, " | "))
+							os.Exit(0)
+						}
+					}
+				}
+			}
+		}
+	}
+
+}
